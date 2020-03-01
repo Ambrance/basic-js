405c4fa4ba5fe27e9850815bfdbfc68f4a6e1a90
Ambrance <ambrance@mail.ru>
2020-03-01 11:32:22 +0300

    feat: pass a half of tests extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,20 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let res = str;
+    if (options) {
+        const times = options.repeatTimes || 1;
+        let addition = options.addition || '';
+        const separator = options.separator || '+';
+        const additionRepeat = options.additionRepeatTimes || 1;
+        const additionSeparator = options.additionSeparator || '|';
+        let additionPattern = addition;
+        if (additionRepeat > 1) {
+            additionPattern = additionSeparator + addition;
+        }
+        const pattern = str + additionPattern.repeat(additionRepeat);
+        res += addition + additionPattern.repeat(additionRepeat - 1);
+        for (let i = 1; i < times; i++) {
+            res += separator + pattern;
+        }
+    }
+    return res;
 };
-  
