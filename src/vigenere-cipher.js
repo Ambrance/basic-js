246159ae29f25d2e700e8530628ab9274e461c63
Ambrance <ambrance@mail.ru>
2020-03-02 16:48:23 +0300

    feat: Vigenere cipher task started

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,54 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(modeMarker) {
+        mode: modeMarker;
+    }
+    genTable() {
+        let table = [];
+        const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$(),./|*-&^%';
+        for (let i = 0; i < alphabet.length; i++) {
+            let row = alphabet.slice(i);
+            row += alphabet.slice(0, i);
+            table.push(row);
+        }
+        console.log(table);
+        return table;
+    }
+    getKeyScript(str1, str2) {
+        let res = '';
+        let k = 0;
+        for (let i = 0; i < str2.length; i++) {
+            if (i % str1.length === 0) {
+                k = 0;
+            }
+            res += str1[k];
+            k++;
+        }
+        return res;
+    }
+    encrypt(message, key) {
+        if (!message || !key) {
+            throw new Error();
+        }
+        let string = "";
+        key.toUpperCase();
+        message.toUpperCase();
+        let keyScript = this.getKeyScript(key, message);
+        let table = this.genTable();
+        const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$(),./|*-&^%';
+        for (let k = 0; k < message.length; k++) {
+            let i = alphabet.indexOf(message[k]);
+            let j = alphabet.indexOf(keyScript[k]);
+            string += table[i][j];
+        }
+        return this.mode ? string.reverse() : string;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    decrypt(encryptedMessage, key) {
+        if (!encryptedMessage || !key) {
+            throw new Error();
+        }
+        let string = '';
+        return this.mode ? string.reverse() : string;
     }
 }
 
